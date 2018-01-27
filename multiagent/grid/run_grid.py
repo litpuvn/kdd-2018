2db09c2a27511ff8cb3f8ec51c108695cb995f88
Hoang Long <litpuvn@gmail.com>
2018-01-27 12:55:24 -0600

    add q learning agent

---
+++ multiagent/grid/run_grid.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+import os,sys
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+import argparse
+
+from multiagent.environment import MultiAgentEnv
+from multiagent.policy import InteractivePolicy
+import multiagent.scenarios as scenarios
+from multiagent.speaker_listener import SpeakerListenerScenario
+
+
+from multiagent.grid.grid_env import Env
+from multiagent.grid.q_learning_agent import QLearningAgent
+
+# if __name__ == '__main__':
+#
+#
+#     # load scenario from script
+#     scenario = SpeakerListenerScenario()
+#     # create world
+#     world = scenario.make_world()
+#     # create multiagent environment
+#     env = MultiAgentEnv(world, scenario.reset_world, scenario.reward, scenario.observation, info_callback=None, shared_viewer = False)
+#     # render call to create viewer window (necessary only for interactive policies)
+#     env.render()
+#     # create interactive policies for each agent
+#     policies = [InteractivePolicy(env,i) for i in range(env.n)]
+#     # execution loop
+#     obs_n = env.reset()
+#     while True:
+#         # query for action from each agent's policy
+#         act_n = []
+#         for i, policy in enumerate(policies):
+#             act_n.append(policy.action(obs_n[i]))
+#         # step environment
+#         obs_n, reward_n, done_n, _ = env.step(act_n)
+#         # render all agent views
+#         env.render()
+#         # display rewards
+#         #for agent in env.world.agents:
+#         #    print(agent.name + " reward: %0.3f" % env._get_reward(agent))
+
+
+
+if __name__ == "__main__":
+    env = Env()
+    agent = QLearningAgent(actions=list(range(env.n_actions)))
+
+    for episode in range(1000):
+        state = env.reset()
+        print("Episode", episode)
+
+        while True:
+            env.render()
+
+            # take action and proceed one step in the environment
+            action = agent.get_action(str(state))
+            next_state, reward, done = env.step(action)
+
+            # with sample <s,a,r,s'>, agent learns new q function
+            agent.learn(str(state), action, reward, str(next_state))
+
+            state = next_state
+            env.print_value_all(agent.q_table)
+
+            # if episode ends, then break
+            if done:
+                break
