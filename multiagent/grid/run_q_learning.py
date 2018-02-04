fe32df3f538c40b45f1f13e532395ff26f0d79d1
Hoang Long <litpuvn@gmail.com>
2018-02-04 11:39:42 -0600

    add more log

--- multiagent/grid/run_q_learning.py
+++ multiagent/grid/run_q_learning.py
@@ -17,34 +17,6 @@ from multiagent.grid.greedy_agent import GreedyAgent
 from multiagent.grid.deep_reinforce_agent import DeepReinforceAgent
 import pylab
 
-# if __name__ == '__main__':
-#
-#
-#     # load scenario from script
-#     scenario = SpeakerListenerScenario()
-#     # create world
-#     world = scenario.make_world()
-#     # create multiagent environment
-#     env = MultiAgentEnv(world, scenario.reset_world, scenario.reward, scenario.observation, info_callback=None, shared_viewer = False)
-#     # render call to create viewer window (necessary only for interactive policies)
-#     env.render()
-#     # create interactive policies for each agent
-#     policies = [InteractivePolicy(env,i) for i in range(env.n)]
-#     # execution loop
-#     obs_n = env.reset()
-#     while True:
-#         # query for action from each agent's policy
-#         act_n = []
-#         for i, policy in enumerate(policies):
-#             act_n.append(policy.action(obs_n[i]))
-#         # step environment
-#         obs_n, reward_n, done_n, _ = env.step(act_n)
-#         # render all agent views
-#         env.render()
-#         # display rewards
-#         #for agent in env.world.agents:
-#         #    print(agent.name + " reward: %0.3f" % env._get_reward(agent))
-
 TOTAL_EPISODES = 2500
 
 if __name__ == "__main__":
@@ -73,10 +45,11 @@ if __name__ == "__main__":
 
     for episode in range(TOTAL_EPISODES):
         state_n = env.reset_n()
-        print("Episode", episode, "states:", state_n)
         counter = 0
         cumulative_reward = 0
         score = 0
+
+        episode_time_step = 0
 
         while True:
             env.render()
@@ -85,6 +58,7 @@ if __name__ == "__main__":
             counter = counter + 1
             # take action and proceed one step in the environment
             global_step += 1
+            episode_time_step += 1
             action_n = policy.get_action_n(state_n)
             next_state_n = []
 
@@ -98,9 +72,6 @@ if __name__ == "__main__":
 
                 cumulative_reward += reward
                 score += reward
-                # print("Episode=", episode, ", agent=", agent.get_id(),  ", at iteration=", counter, ", with total reward=", cumulative_reward)
-
-                # env.print_value_all(agent.q_table)
 
             policy.learn(state_n, action_n, reward_n, next_state_n)
             state_n = copy.deepcopy(next_state_n)
@@ -110,7 +81,7 @@ if __name__ == "__main__":
                 scores.append(score)
                 episodes.append(episode)
 
-                print("episode:", episode, "  score:", score, "  time_step:", global_step)
+                print("episode:", episode, "  score:", score, "  episodetime_step:", episode_time_step, " global time:", global_step)
 
                 # print("Episode=", episode, ", ends in a number of iterations=", counter, ", with total reward=", cumulative_reward)
                 break
