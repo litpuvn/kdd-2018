39581fb6eb8d3aaa35c4aa6e23bb95f181deed89
Hoang Long <litpuvn@gmail.com>
2018-02-06 12:01:52 -0600

    add more agents

--- multiagent/grid/run_q_learning.py
+++ multiagent/grid/run_q_learning.py
@@ -25,8 +25,8 @@ if __name__ == "__main__":
 
     env = Env(max_agent_count, max_victim_count)
 
-    agent_count = 1
-    victim_count = 3
+    agent_count = 2
+    victim_count = 4
 
     # for i in range(agent_count):
     #     agent = QLearningAgent(actions=list(range(env.n_actions)), agent_id=i, env=env)
@@ -34,12 +34,15 @@ if __name__ == "__main__":
     agent = QLearningAgent(actions=list(range(env.n_actions)), agent_id=0, env=env)
     env.add_agent_at_pos(agent, 0)
 
+    agent2 = QLearningAgent(actions=list(range(env.n_actions)), agent_id=1, env=env)
+    env.add_agent_at_pos(agent2, 20)
+
     # for i in range(victim_count):
     #     env.add_victim()
     env.add_victim_at_pos(7, 100)
     env.add_victim_at_pos(11, 100)
     env.add_victim_at_pos(12, 100)
-    # env.add_victim_at_pos(24, 100)
+    env.add_victim_at_pos(24, 100)
 
     env.pack_canvas()
 
